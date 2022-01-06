85eef4d289c578dc1b8054443e0b08aa4c2348d6
Mark Dale <msdaledad@gmail.com>
2022-01-06 11:57:00 -0500

    added some new unfinished...but moving to another branch

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
+const { promptTrackerAction } = require('./utils/tracker-inquery');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
